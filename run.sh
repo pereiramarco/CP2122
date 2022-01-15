06f0a74e84b61b4e5521cae115046f70ffd1df5b
MrMarisco <marcopereiravpa@gmail.com>
2022-01-15 16:40:26 +0000

    better run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#directory
 MY=~/CP2122
 
 #size of the random array
@@ -11,5 +12,12 @@ MAX_NUMBER=100000000
 #type of the run executed (0 for parallel, 1 for sequential)
 RUN_TYPE=0
 
+#node used for running the program
+NODE=compute-134-135
+
+#number of threads
+NUM_THREADS=2
+
 module load papi/5.4.1
-srun --partition=cpar $MY/sort $SORT_SIZE $MAX_NUMBER $RUN_TYPE
+setenv OMP_NUM_THREADS $NUM_THREADS
+srun --partition=cpar --nodelist=$NODE $MY/sort $SORT_SIZE $MAX_NUMBER $RUN_TYPE
